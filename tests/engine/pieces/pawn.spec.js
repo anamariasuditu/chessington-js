5f916ee270c04a05d7e18f35d26f682e5d698358
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 14:33:06 +0100

    Added a test that pawns cannot move through other pieces

--- tests/engine/pieces/pawn.spec.js
+++ tests/engine/pieces/pawn.spec.js
@@ -1,5 +1,6 @@
 import 'chai/register-should';
 import Pawn from '../../../src/engine/pieces/pawn';
+import Rook from '../../../src/engine/pieces/rook';
 import Board from '../../../src/engine/board';
 import Player from '../../../src/engine/player';
 import Square from '../../../src/engine/square';
