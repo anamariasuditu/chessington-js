25b832f32426c8ce8372b9f89c39f4491fc61389
AnaSud <Ana-Maria.Suditu@softwire.com>
2025-07-07 17:37:32 +0300

    move queen laterally and diagonally

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Queen extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,33 @@ export default class Queen extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves = []
+        let currentSquare = board.findPiece(this);
+        let nextSquare;
+        const directions = [
+            {row: 1, col: 0}, //sus
+            {row: -1, col: 0}, //jos
+            {row: 0, col: -1}, //stanga
+            {row: 0, col: 1}, //dreapta
+            {row: 1, col: 1}, //sus-dreapta
+            {row: 1, col: -1}, //sus-stanga
+            {row: -1, col: 1}, // jos-dreapta
+            {row: -1, col: -1}, //jos-stanga
+        ];
+
+        for(const direction of directions){
+            let nextRow = currentSquare.row + direction.row;
+            let nextCol = currentSquare.col + direction.col;
+
+            nextSquare = Square.at(nextRow, nextCol);
+            while(nextSquare.isValidSquare(nextRow,nextCol)){
+                moves.push(Square.at(nextRow,nextCol));
+                nextRow += direction.row ;
+                nextCol += direction.col;
+            }
+        }
+
+        return moves;
+
     }
 }
