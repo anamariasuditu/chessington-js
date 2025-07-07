35e809e364f63263af20fff30ab5e046a7cacb0d
AnaSud <Ana-Maria.Suditu@softwire.com>
2025-07-07 16:58:28 +0300

    move rook laterally

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,23 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves = []
+        let currentSquare = board.findPiece(this);
+
+        //move vertically
+        for(let row = 0; row < 8; row++){
+            if(currentSquare.row!= row){
+                moves.push(Square.at(row, currentSquare.col));
+            }
+        }
+
+        //move horizontally
+        for(let col = 0; col < 8; col++){
+            if(currentSquare.col!= col){
+                moves.push(Square.at(currentSquare.row, col));
+            }
+        }
+
+        return moves;
     }
 }
