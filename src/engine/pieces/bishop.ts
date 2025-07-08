65f70714b14c31359553f72aa59198b43251430a
AnaSud <Ana-Maria.Suditu@softwire.com>
2025-07-08 11:19:54 +0300

    cannot move bishop through pieces

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -24,10 +24,16 @@ export default class Bishop extends Piece {
             let nextCol = currentSquare.col + direction.col;
 
             nextSquare = Square.at(nextRow, nextCol);
+
             while(nextSquare.isValidSquare(nextRow,nextCol)){
-                moves.push(Square.at(nextRow,nextCol));
-                nextRow += direction.row ;
-                nextCol += direction.col;
+                nextSquare = Square.at(nextRow, nextCol);
+                let piece = board.getPiece(nextSquare);
+                if(!piece){
+                    moves.push(Square.at(nextRow,nextCol));
+                    nextRow += direction.row ;
+                    nextCol += direction.col;
+                } else break;
+
             }
         }
         return moves;
